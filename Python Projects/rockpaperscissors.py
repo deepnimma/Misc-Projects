14bbc511901e68ab7f6e651e34c9119d508dec04
Deepesh Nimma <56356841+NottCurious@users.noreply.github.com>
2020-12-10 18:28:52 +0530

    Updating Repositories

---
+++ Python Projects/rockpaperscissors.py
@@ -0,0 +1,27 @@
+import random
+
+b_score = 0
+u_score = 0
+
+
+def printScreen(bot_score, user_score):
+    print("Bot Score: ", bot_score, " User Score: ", user_score)
+
+
+def getWinner(bot_score, my_score):
+    if bot_score == 3:
+        return bot_winner
+    elif my_score == 3:
+        return user_winner
+    else:
+        return cont
+
+
+def printscore(bot_score, my_score):
+
+
+def main():
+
+
+while True:
+    main()
--- Website Development/Counting Project/script.js
+++
@@ -1,8 +0,0 @@
-const BUTTON = document.getElementById("btn");
-const counter = document.getElementById("questions_done");
-var questionsDone = 0;
-
-document.addEventListener("click", () => {
-    questionsDone += 5;
-    counter.innerHTML = questionsDone;
-});
